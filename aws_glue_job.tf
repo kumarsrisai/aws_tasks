752dc033e876a4c5bdefd8603773cd63a7b5eaee
Kumar Sri Sai Kottagundu <kumarsrisai08@gmail.com>
2024-09-04 22:35:13 +0530

    commeneted

--- aws_glue_job.tf
+++ aws_glue_job.tf
@@ -1,82 +1,82 @@
-#AWS Glue job for a Python script
-resource "aws_glue_job" "example" {
-  name = "DDSL_Glue_job"
-  role_arn = aws_iam_role.gluerole.arn
-  glue_version = "4.0"  
-  number_of_workers = "2.0"
-  worker_type = "G.1X"
-  command {
-    #name            = "pythonshell"
-    script_location = "s3://${aws_s3_bucket.example1.bucket}/segregate.py"
-    python_version = "3"
-  }
-   default_arguments = {    
-    "--continuous-log-logGroup"          = aws_cloudwatch_log_group.glue_job_log_group.name
-    "--enable-continuous-cloudwatch-log" = "true"
-    "--enable-continuous-log-filter"     = "true"
-    "--enable-metrics"                   = ""
-  }
-}
+# #AWS Glue job for a Python script
+# resource "aws_glue_job" "example" {
+#   name = "DDSL_Glue_job"
+#   role_arn = aws_iam_role.gluerole.arn
+#   glue_version = "4.0"  
+#   number_of_workers = "2.0"
+#   worker_type = "G.1X"
+#   command {
+#     #name            = "pythonshell"
+#     script_location = "s3://${aws_s3_bucket.example1.bucket}/segregate.py"
+#     python_version = "3"
+#   }
+#    default_arguments = {    
+#     "--continuous-log-logGroup"          = aws_cloudwatch_log_group.glue_job_log_group.name
+#     "--enable-continuous-cloudwatch-log" = "true"
+#     "--enable-continuous-log-filter"     = "true"
+#     "--enable-metrics"                   = ""
+#   }
+# }
 
-#AWS Glue job for a Py script
-resource "aws_glue_job" "data_quality1" {
-  name = "DDSL_Dataquality1_job"
-  role_arn = aws_iam_role.gluerole.arn
-  max_capacity = "1.0"
-  glue_version = "4.0"
-  command {
-    #name            = "pythonshell"
-    script_location = "s3://${aws_s3_bucket.example1.bucket}/dq1.py"
-    python_version = "3"
-  }
-   default_arguments = {    
-    "--continuous-log-logGroup"          = aws_cloudwatch_log_group.data_quality_log_group1.name
-    "--enable-continuous-cloudwatch-log" = "true"
-    "--enable-continuous-log-filter"     = "true"
-    "--enable-metrics"                   = ""
-  }
-}
+# #AWS Glue job for a Py script
+# resource "aws_glue_job" "data_quality1" {
+#   name = "DDSL_Dataquality1_job"
+#   role_arn = aws_iam_role.gluerole.arn
+#   max_capacity = "1.0"
+#   glue_version = "4.0"
+#   command {
+#     #name            = "pythonshell"
+#     script_location = "s3://${aws_s3_bucket.example1.bucket}/dq1.py"
+#     python_version = "3"
+#   }
+#    default_arguments = {    
+#     "--continuous-log-logGroup"          = aws_cloudwatch_log_group.data_quality_log_group1.name
+#     "--enable-continuous-cloudwatch-log" = "true"
+#     "--enable-continuous-log-filter"     = "true"
+#     "--enable-metrics"                   = ""
+#   }
+# }
 
-#AWS Glue job for a Py script
-resource "aws_glue_job" "data_quality2" {
-  name = "DDSL_Dataquality2_job"
-  role_arn = aws_iam_role.gluerole.arn
-  max_capacity = "1.0"
-  glue_version = "4.0"
-  command {
-    #name            = "pythonshell"
-    script_location = "s3://${aws_s3_bucket.example1.bucket}/dq2.py"
-    python_version = "3"
-  }
-   default_arguments = {    
-    "--continuous-log-logGroup"          = aws_cloudwatch_log_group.data_quality_log_group2.name
-    "--enable-continuous-cloudwatch-log" = "true"
-    "--enable-continuous-log-filter"     = "true"
-    "--enable-metrics"                   = ""
-  }
-}
+# #AWS Glue job for a Py script
+# resource "aws_glue_job" "data_quality2" {
+#   name = "DDSL_Dataquality2_job"
+#   role_arn = aws_iam_role.gluerole.arn
+#   max_capacity = "1.0"
+#   glue_version = "4.0"
+#   command {
+#     #name            = "pythonshell"
+#     script_location = "s3://${aws_s3_bucket.example1.bucket}/dq2.py"
+#     python_version = "3"
+#   }
+#    default_arguments = {    
+#     "--continuous-log-logGroup"          = aws_cloudwatch_log_group.data_quality_log_group2.name
+#     "--enable-continuous-cloudwatch-log" = "true"
+#     "--enable-continuous-log-filter"     = "true"
+#     "--enable-metrics"                   = ""
+#   }
+# }
 
-#AWS Glue job for a Py script
-resource "aws_glue_job" "data_lineage" {
-  name = "DDSL_Datalineage_job"
-  role_arn = aws_iam_role.gluerole.arn  
-  glue_version = "4.0"
-  number_of_workers = "2.0"
-  worker_type = "G.1X"
-  command {
-    #name            = "pythonshell"
-    script_location = "s3://${aws_s3_bucket.example1.bucket}/lineage.py"
-    python_version = "3"
-  }
-   default_arguments = {    
-    "--continuous-log-logGroup"          = aws_cloudwatch_log_group.data_lineage.name
-    "--enable-continuous-cloudwatch-log" = "true"
-    "--enable-continuous-log-filter"     = "true"
-    "--enable-metrics"                   = ""
-    "--job-language"                     = "Python 3"
-    "--scriptLocation"                   = "s3://${aws_s3_bucket.example1.bucket}/lineage.py"
-    "--extra-jars"                       = "s3://${aws_s3_bucket.example1.bucket}/openlineage-spark_2.12-1.13.1.jar,"
-    "--user-jars-first"                  = "true" 
-    "--encryption-type"                  = ""
-  }
-}
+# #AWS Glue job for a Py script
+# resource "aws_glue_job" "data_lineage" {
+#   name = "DDSL_Datalineage_job"
+#   role_arn = aws_iam_role.gluerole.arn  
+#   glue_version = "4.0"
+#   number_of_workers = "2.0"
+#   worker_type = "G.1X"
+#   command {
+#     #name            = "pythonshell"
+#     script_location = "s3://${aws_s3_bucket.example1.bucket}/lineage.py"
+#     python_version = "3"
+#   }
+#    default_arguments = {    
+#     "--continuous-log-logGroup"          = aws_cloudwatch_log_group.data_lineage.name
+#     "--enable-continuous-cloudwatch-log" = "true"
+#     "--enable-continuous-log-filter"     = "true"
+#     "--enable-metrics"                   = ""
+#     "--job-language"                     = "Python 3"
+#     "--scriptLocation"                   = "s3://${aws_s3_bucket.example1.bucket}/lineage.py"
+#     "--extra-jars"                       = "s3://${aws_s3_bucket.example1.bucket}/openlineage-spark_2.12-1.13.1.jar,"
+#     "--user-jars-first"                  = "true" 
+#     "--encryption-type"                  = ""
+#   }
+# }
--- aws_step_function.tf
+++ aws_step_function.tf
@@ -1,44 +1,44 @@
-// Create state machine for step function
-resource "aws_sfn_state_machine" "sfn_state_machine" {
-  name     = "sample-state-machine" 
+# // Create state machine for step function
+# resource "aws_sfn_state_machine" "sfn_state_machine" {
+#   name     = "sample-state-machine" 
   
-  role_arn = aws_iam_role.iam_for_sfn.arn
-  type     = "STANDARD"
-  definition = <<EOF
-{
-    "Comment": "To trigger events from s3 to stepfunction to AWS Glue",
-    "StartAt": "GlueJob",
-    "States": {
-        "GlueJob": {
-            "Type": "Task",
-            "Resource": "arn:aws:states:::glue:startJobRun.sync",
-            "Parameters": {
-                "JobName": "${aws_glue_job.example.name}"
-            },
-            "Next": "Passed Record Job run"
-        },
-        "Passed Record Job run": {
-                    "Type": "Task",
-                    "Resource": "arn:aws:states:::glue:startJobRun.sync",
-                    "Parameters": {
-                        "JobName": "${aws_glue_job.data_quality1.name}"
-                    },
-                    "Next": "Checksum Record Job run"
-                },
-        "Checksum Record Job run": {
-                    "Type": "Task",
-                    "Resource": "arn:aws:states:::glue:startJobRun.sync",
-                    "Parameters": {
-                        "JobName": "${aws_glue_job.data_quality2.name}"
-                    },
-                    "End": true
-                }
-    }
-}
-EOF
-logging_configuration {  
-  log_destination = "${aws_cloudwatch_log_group.stepfunction_log_group.arn}:*"
-  include_execution_data = true
-  level = "ALL"  
-}
-}  
+#   role_arn = aws_iam_role.iam_for_sfn.arn
+#   type     = "STANDARD"
+#   definition = <<EOF
+# {
+#     "Comment": "To trigger events from s3 to stepfunction to AWS Glue",
+#     "StartAt": "GlueJob",
+#     "States": {
+#         "GlueJob": {
+#             "Type": "Task",
+#             "Resource": "arn:aws:states:::glue:startJobRun.sync",
+#             "Parameters": {
+#                 "JobName": "${aws_glue_job.example.name}"
+#             },
+#             "Next": "Passed Record Job run"
+#         },
+#         "Passed Record Job run": {
+#                     "Type": "Task",
+#                     "Resource": "arn:aws:states:::glue:startJobRun.sync",
+#                     "Parameters": {
+#                         "JobName": "${aws_glue_job.data_quality1.name}"
+#                     },
+#                     "Next": "Checksum Record Job run"
+#                 },
+#         "Checksum Record Job run": {
+#                     "Type": "Task",
+#                     "Resource": "arn:aws:states:::glue:startJobRun.sync",
+#                     "Parameters": {
+#                         "JobName": "${aws_glue_job.data_quality2.name}"
+#                     },
+#                     "End": true
+#                 }
+#     }
+# }
+# EOF
+# logging_configuration {  
+#   log_destination = "${aws_cloudwatch_log_group.stepfunction_log_group.arn}:*"
+#   include_execution_data = true
+#   level = "ALL"  
+# }
+# }  
