8169711db43281378b30ff4591fb236e774d7478
Kumar Sri Sai Kottagundu <kumarsrisai08@gmail.com>
2024-09-09 12:56:43 +0530

    updated cloudtrail policy in data.tf

--- data.tf
+++ data.tf
@@ -18,20 +18,53 @@
 #   }
 # }
 
+# resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
+#   bucket = aws_s3_bucket.data_bucket.id
+
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Sid       = "AWSCloudTrailWrite"
+#         Effect    = "Allow"
+#         Principal = {
+#           Service = "cloudtrail.amazonaws.com"
+#         }
+#         Action    = "s3:PutObject"
+#         Resource  = "${aws_s3_bucket.data_bucket.arn}/*"
+#         Condition = {
+#           StringEquals = {
+#             "s3:x-amz-acl" = "bucket-owner-full-control"
+#           }
+#         }
+#       }
+#     ]
+#   })
+# }
+
 resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
-  bucket = aws_s3_bucket.data_bucket.id
+  bucket = aws_s3_bucket.ddsl_raw_extended_developer.id
 
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Sid       = "AWSCloudTrailWrite"
-        Effect    = "Allow"
+        Sid       = "AWSCloudTrailAclCheck",
+        Effect    = "Allow",
         Principal = {
           Service = "cloudtrail.amazonaws.com"
-        }
-        Action    = "s3:PutObject"
-        Resource  = "${aws_s3_bucket.data_bucket.arn}/*"
+        },
+        Action    = "s3:GetBucketAcl",
+        Resource  = "${aws_s3_bucket.ddsl_raw_extended_developer.arn}"
+      },
+      {
+        Sid       = "AWSCloudTrailWrite",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action    = "s3:PutObject",
+        Resource  = "${aws_s3_bucket.ddsl_raw_extended_developer.arn}/*",
         Condition = {
           StringEquals = {
             "s3:x-amz-acl" = "bucket-owner-full-control"
@@ -41,6 +74,7 @@ resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
     ]
   })
 }
+
 
 
 
