8ea2034d16ad3631e112d807f3a6c2d7cbd991ef
Kumar Sri Sai Kottagundu <kumarsrisai08@gmail.com>
2024-09-17 16:37:50 +0530

    updated step function

--- sample.py
+++ sample.py
@@ -2,13 +2,7 @@ resource "aws_sfn_state_machine" "sfn_state_machine" {
   name     = "ddsl-sfn-state-machine-developer"
   role_arn = aws_iam_role.iam_for_sfn.arn
   type     = "STANDARD"
-  logging_configuration {
-    log_destination = "${aws_cloudwatch_log_group.stepfunction_log_group.arn}:*"
-    include_execution_data = true
-    level = "ALL"
-  }
   definition = <<EOF
-  
 {
     "Comment": "To trigger events from S3 to StepFunction to AWS Glue",
     "StartAt": "Segregate",
@@ -19,6 +13,7 @@ resource "aws_sfn_state_machine" "sfn_state_machine" {
             "Parameters": {
                 "JobName": "${aws_glue_job.segregate.name}",
                 "Arguments": {
+                   
                     "--env": "dev"
                 }
             },
@@ -36,7 +31,8 @@ resource "aws_sfn_state_machine" "sfn_state_machine" {
             },
             "Next": "Checksum Record Job run"
         },
-        "Checksum Record Job run": {
+        "Checksum Record Job run": 
+        {
             "Type": "Parallel",
             "Branches": [
               {
@@ -46,7 +42,7 @@ resource "aws_sfn_state_machine" "sfn_state_machine" {
                     "Type": "Task",
                     "Resource": "arn:aws:states:::glue:startJobRun.sync",
                     "Parameters": {
-                      "JobName": "${aws_glue_job.data_quality2.name}",
+                      "JobName": "${aws_glue_job.data_quality2.name}"
                       "Arguments": {
                         "--rec_type": "rec_type_9000",
                         "--env": "dev"
@@ -257,76 +253,171 @@ resource "aws_sfn_state_machine" "sfn_state_machine" {
                     "End": true
                   }
                 }
+              },
+              {
+                "StartAt": "Record level validations for 9012",
+                "States": {
+                  "Record level validations for 9012": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::glue:startJobRun.sync",
+                    "Parameters": {
+                      "JobName": "${aws_glue_job.data_quality2.name}",
+                      "Arguments": {
+                        "--rec_type": "rec_type_9012",
+                        "--env": "dev"
+                      }
+                    },
+                    "Next": "History updates for 9012"
+                  },
+                  "History updates for 9012": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::glue:startJobRun.sync",
+                    "Parameters": {
+                      "JobName": "${aws_glue_job.data_history.name}",
+                      "Arguments": {
+                        "--rec_type": "rec_type_9012",
+                        "--env": "dev",
+                        "--datalake-formats": "hudi",
+                        "-Djdk.attach.allowAttachSelf": "true",
+                        "--conf": "spark.serializer=org.apache.spark.serializer.KryoSerializer --conf spark.sql.parquet.datetimeRebaseModeInRead=CORRECTED --conf spark.sql.shuffle.partitions=5"
+                      }
+                    },
+                    "End": true
+                  }
+                }
+              },
+              {
+                "StartAt": "Record level validations for 9019",
+                "States": {
+                  "Record level validations for 9019": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::glue:startJobRun.sync",
+                    "Parameters": {
+                      "JobName": "${aws_glue_job.data_quality2.name}",
+                      "Arguments": {
+                        "--rec_type": "rec_type_9019",
+                        "--env": "dev"
+                      }
+                    },
+                    "Next": "History load for 9019"
+                  },
+                  "History load for 9019": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::glue:startJobRun.sync",
+                    "Parameters": {
+                      "JobName": "sb_History_Load-dev1_v2",
+                      "Arguments": {
+                        "--rec_type": "rec_type_9019",
+                        "--env": "dev",
+                        "--datalake-formats": "hudi",
+                        "-Djdk.attach.allowAttachSelf": "true",
+                        "--conf": "spark.serializer=org.apache.spark.serializer.KryoSerializer --conf spark.sql.parquet.datetimeRebaseModeInRead=CORRECTED --conf spark.sql.shuffle.partitions=5"
+                      }
+                    },
+                    "Next": "Transposed for 9019"
+                  },
+                  "Transposed for 9019": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::glue:startJobRun.sync",
+                    "Parameters": {
+                      "JobName": "sb_Rec_type_Transpose",
+                      "Arguments": {
+                        "--rec_type": "rec_type_9019",
+                        "--env": "dev"
+                      }
+                    },
+                    "Next": "History updates for 9019"
+                  },
+                  "History updates for 9019": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::glue:startJobRun.sync",
+                    "Parameters": {
+                      "JobName": "${aws_glue_job.data_history.name}",
+                      "Arguments": {
+                        "--rec_type": "rec_type_9019",
+                        "--env": "dev",
+                        "--datalake-formats": "hudi",
+                        "-Djdk.attach.allowAttachSelf": "true",
+                        "--conf": "spark.serializer=org.apache.spark.serializer.KryoSerializer --conf spark.sql.parquet.datetimeRebaseModeInRead=CORRECTED --conf spark.sql.shuffle.partitions=5"
+                      }
+                    },
+                    "End": true
+                  }
+                }
+              },
+              {
+                "StartAt": "Record level validations for 9020",
+                "States": {
+                  "Record level validations for 9020": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::glue:startJobRun.sync",
+                    "Parameters": {
+                      "JobName": "${aws_glue_job.data_quality2.name}",
+                      "Arguments": {
+                        "--rec_type": "rec_type_9020",
+                        "--env": "dev"
+                      }
+                    },
+                    "Next": "History updates for 9020"
+                  },
+                  "History updates for 9020": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::glue:startJobRun.sync",
+                    "Parameters": {
+                      "JobName": "${aws_glue_job.data_history.name}",
+                      "Arguments": {
+                        "--rec_type": "rec_type_9020",
+                        "--env": "dev",
+                        "--datalake-formats": "hudi",
+                        "-Djdk.attach.allowAttachSelf": "true",
+                        "--conf": "spark.serializer=org.apache.spark.serializer.KryoSerializer --conf spark.sql.parquet.datetimeRebaseModeInRead=CORRECTED --conf spark.sql.shuffle.partitions=5"
+                      }
+                    },
+                    "End": true
+                  }
+                }
+              },
+              {
+                "StartAt": "Record level validations for 9031",
+                "States": {
+                  "Record level validations for 9031": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::glue:startJobRun.sync",
+                    "Parameters": {
+                      "JobName": "${aws_glue_job.data_quality2.name}",
+                      "Arguments": {
+                        "--rec_type": "rec_type_9031",
+                        "--env": "dev"
+                      }
+                    },
+                    "Next": "History updates for 9031"
+                  },
+                  "History updates for 9031": {
+                    "Type": "Task",
+                    "Resource": "arn:aws:states:::glue:startJobRun.sync",
+                    "Parameters": {
+                      "JobName": "${aws_glue_job.data_history.name}",
+                      "Arguments": {
+                        "--rec_type": "rec_type_9031",
+                        "--env": "dev",
+                        "--datalake-formats": "hudi",
+                        "-Djdk.attach.allowAttachSelf": "true",
+                        "--conf": "spark.serializer=org.apache.spark.serializer.KryoSerializer --conf spark.sql.parquet.datetimeRebaseModeInRead=CORRECTED --conf spark.sql.shuffle.partitions=5"
+                      }
+                    },
+                    "End": true
+                  }
+                }
               }
-            ]
-        }
+            ],
+            "Next": "FinalState"
+          }
+          EOF
     }
 }
-EOF
+  logging_configuration {  
+    log_destination = "${aws_cloudwatch_log_group.stepfunction_log_group.arn}:*"
+    include_execution_data = true
+    level = "ALL"
+  }
 }
-
-
-   
-
-
-
-
-
-
-
-
-
-# {
-#     "Comment": "To trigger events from S3 to StepFunction to AWS Glue",
-#     "StartAt": "Segregate",
-#     "States": {
-#         "Segregate": {
-#             "Type": "Task",
-#             "Resource": "arn:aws:states:::glue:startJobRun.sync",
-#             "Parameters": {
-#                 "JobName": "${aws_glue_job.segregate.name}",
-#                 "Arguments": {
-                   
-#                     "--env": "dev"
-#                 }
-#             },
-#             "Next": "Passed Record Job run"
-#         },
-#         "Passed Record Job run": {
-#             "Type": "Task",
-#             "Resource": "arn:aws:states:::glue:startJobRun.sync",
-#             "Parameters": {
-#                 "JobName": "${aws_glue_job.data_quality1.name}",
-#                 "Arguments": {
-#                     "--rec_type": "rec_type_9000",
-#                     "--env": "dev"
-#                 }
-#             },
-#             "Next": "Checksum Record Job run"
-#         },
-#         "Checksum Record Job run": {
-#             "Type": "Task",
-#             "Resource": "arn:aws:states:::glue:startJobRun.sync",
-#             "Parameters": {
-#                 "JobName": "${aws_glue_job.data_quality2.name}",
-#                 "Arguments": {
-#                     "--rec_type": "rec_type_9000",
-#                     "--env": "dev"
-#                 }
-#             },
-#             "Next": "History level validation Job run"
-#         },
-#         "History level validation Job run": {
-#             "Type": "Task",
-#             "Resource": "arn:aws:states:::glue:startJobRun.sync",
-#             "Parameters": {
-#                 "JobName": "${aws_glue_job.data_history.name}",
-#                 "Arguments": {
-#                     "--rec_type": "rec_type_9000",
-#                     "--env": "dev"
-#                 }
-#             },
-#             "End": true
-#         }
-#     }
-# }
