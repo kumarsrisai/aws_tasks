9d2a5ced7fc5ac805ca51ca2d5b30cae18fa6a0e
Kumar Sri Sai Kottagundu <kumarsrisai08@gmail.com>
2024-09-17 17:02:02 +0530

    updated step function

--- aws_step_function.tf
+++ aws_step_function.tf
@@ -413,11 +413,11 @@ resource "aws_sfn_state_machine" "sfn_state_machine" {
             ],
             "Next": "FinalState"
           }
-          EOF
     }
-
+EOF
 logging_configuration {  
     log_destination = "${aws_cloudwatch_log_group.stepfunction_log_group.arn}:*"
     include_execution_data = true
     level = "ALL"
   }
+}
